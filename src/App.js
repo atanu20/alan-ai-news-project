bca29f8e9ecc3995ae080a775f4e5c2aaa5e1695
ATANU JANA <65564188+atanu20@users.noreply.github.com>
2021-08-05 23:23:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
 import Home from './component/Home';
-
 import './App.css'
 import {BrowserRouter, Switch , Route} from 'react-router-dom';
+
+
 const App = () => {
   return (
     <>
